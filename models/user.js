658768d14da32968d6214d5b338abafba21fac84
Lawrence Snead <Lawrence.Snead@gmail.com>
2020-02-22 11:32:36 -0500

    Fixed user.js stupid mistakes.

--- models/user.js
+++ models/user.js
@@ -6,13 +6,13 @@ module.exports = function(sequelize, DataTypes) {
 
   });
 
-  Author.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    Author.hasMany(models.Post, {
-      onDelete: "cascade"
-    });
-  };
+  // User.associate = function(models) {
+  //   // Associating Author with Posts
+  //   // When an Author is deleted, also delete any associated Posts
+  //   User.hasMany(models.Post, {
+  //     onDelete: "cascade"
+  //   });
+  // };
 
   return User;
 };
