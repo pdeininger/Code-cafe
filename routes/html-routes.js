072ee3b21db2b8319d8d728a338720aa6a8d6a14
Lawrence Snead <Lawrence.Snead@gmail.com>
2020-02-22 12:51:35 -0500

    Changed calendar route definition in html routes file.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
   });
 
   // calendar route loads calendar 
-  app.get("/code-cafe-calendar", function(req, res) {
+  app.get("/calendar", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/calendar.html"));
   });
 
